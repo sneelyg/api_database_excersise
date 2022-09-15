b1f999f5f20d1a18da258bcf5612519db5676a9a
sneelyg <santiagoneely@gmail.com>
2022-09-15 00:08:13 +0000

    favorites added

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, Characters, Episodes
+from models import db, User, Characters, Episodes, Fav_characters, Fav_episodes
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,6 +13,8 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Characters, db.session))
     admin.add_view(ModelView(Episodes, db.session))
+    admin.add_view(ModelView(Fav_characters, db.session))
+    admin.add_view(ModelView(Fav_episodes, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/models.py
+++ src/models.py
@@ -27,7 +27,7 @@ class Characters (db.Model):
     species = db.Column(db.String(30), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.name
+        return '<Characters %r>' % self.name
 
     def serialize(self):
         return {
@@ -39,16 +39,48 @@ class Characters (db.Model):
 
 class Episodes (db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), unique=False, nullable=False)
+    title = db.Column(db.String(120), unique=True, nullable=False)
     air_date = db.Column(db.String(20), unique=False, nullable=False)
     alive = db.Column(db.Boolean(), unique=False, nullable=False)
     species = db.Column(db.String(30), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.title
+        return '<Episodes %r>' % self.title
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.title
+        }
+
+class Fav_characters (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email =  db.Column(db.String(120), db.ForeignKey('user.email'))
+    char_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    rel_user = db.relationship('User')
+    rel_char = db.relationship('Characters')
+        
+    def __repr__(self):
+        return '<Fav_Character %r>' % self.email
+
+    def serialize(self):
+        return {
+            "email": self.email,
+            "Char_ID": self.char_id
+        }
+
+class Fav_episodes (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email =  db.Column(db.String(120), db.ForeignKey('user.email'))
+    title = db.Column(db.String(120), db.ForeignKey('episodes.title'))
+    rel_user = db.relationship('User')
+    rel_episodes = db.relationship('Episodes')
+        
+    def __repr__(self):
+        return '<Fav_Episodes %r>' % self.email
+
+    def serialize(self):
+        return {
+            "email": self.email,
+            "Title": self.title
         }
